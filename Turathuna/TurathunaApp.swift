6d19c6b54f7d6154c91fa7a6c272e37fcf5a9e55
LamiaAlsiddiqi <lwalsiddiqi22@twq.idserve.net>
2022-12-07 10:20:45 +0300

    Initial Commit

---
+++ Turathuna/TurathunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurathunaApp.swift
+//  Turathuna
+//
+//  Created by Lamia AlSiddiqi on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TurathunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
